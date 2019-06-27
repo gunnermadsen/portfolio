d705de9283d8c093c78476de193dad337a627f82
gunnermadsen <gunner.madsen@outlook.com>
2019-06-27 13:17:40 -0700

    updating redirect middleware

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/portfolio'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
-});
-
 app.use(function(req, res, next) {
-    if (!req.secure) {
+    if (!req.secure && process.env.NODE_ENV === 'production') {
         const secureUrl = `https://${req.headers['host']}${req.url}`;
+        console.log(secureUrl);
         // res.writeHead(301, { "Location": secureUrl })
         res.redirect(secureUrl);
         // res.end();
     }
     next();
 })
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);

aad879aefd5547796dc6c050d18a27bce752b837
gunnermadsen <gunner.madsen@outlook.com>
2019-10-05 20:58:48 -0700

    Migrated home component from bootstrap 4 grid system to CSS3 grid system

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { ProjectDialogComponent } from '../project-dialog/project-dialog.compone
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   public projects: any[];
